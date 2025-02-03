3681a7886041d07223a4c2511084c72688864716
Jose Gabriel Jiménez <josegabojimenez@gmail.com>
2025-02-02 21:24:03 -0500

    feat: add panic button image

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,6 +6,7 @@ import {emergenciesList} from '@/mocks/emergency';
 // Components
 import EmergencyCard from '@/components/EmergencyCard';
 import SettingsMenu from '@/components/SettingsMenu';
+import Image from 'next/image';
 
 export default function Dashboard() {
 	return (
@@ -17,6 +18,10 @@ export default function Dashboard() {
 			<div className="mt-12 px-4 flex flex-col items-center w-full">
 				<h1 className="text-3xl font-bold text-gray-900 mb-8">En proceso</h1>
 
+				{/* Panic Button */}
+				<div className="bg-red-300 w-[300px] h-[50vh] relative flex justify-center items-center hover:scale-105">
+					<Image src="/images/panic-button.svg" alt="Botón de pánico" fill />
+				</div>
 				{/* Patient Information */}
 				{emergenciesList.map((emergency) => (
 					<EmergencyCard key={emergency.emergencyId} userName={emergency.userName} userPhone={emergency.userPhone} emergencyId={emergency.emergencyId} />
