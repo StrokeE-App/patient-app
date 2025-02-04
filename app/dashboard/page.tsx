b40f9df2d0326bb31a755f1e1151998cb074fbba
Jose Gabriel Jiménez <josegabojimenez@gmail.com>
2025-02-04 09:22:33 -0500

    feat: finish patient homepage

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,32 +1,38 @@
 'use client';
 
-// Mocks
-import {emergenciesList} from '@/mocks/emergency';
-
-// Components
-import EmergencyCard from '@/components/EmergencyCard';
+import {useEffect} from 'react';
 import SettingsMenu from '@/components/SettingsMenu';
 import Image from 'next/image';
+import {StrokeeLogo} from '@/components/StrokeeLogo';
 
 export default function Dashboard() {
+	useEffect(() => {
+		const setVh = () => {
+			const vh = window.innerHeight * 0.01;
+			document.documentElement.style.setProperty('--vh', `${vh}px`);
+		};
+		setVh();
+		window.addEventListener('resize', setVh);
+		return () => window.removeEventListener('resize', setVh);
+	}, []);
+
 	return (
-		<main className="min-h-screen bg-white p-4">
+		<main style={{minHeight: 'calc(var(--vh, 1vh) * 100)'}} className=" p-4 flex flex-col justify-between">
 			{/* Header */}
 			<SettingsMenu />
 
 			{/* Main Content */}
 			<div className="mt-12 px-4 flex flex-col items-center w-full">
-				<h1 className="text-3xl font-bold text-gray-900 mb-8">En proceso</h1>
+				{/* <h1 className="text-3xl font-bold text-gray-900 mb-8">En proceso</h1> */}
 
 				{/* Panic Button */}
-				<div className="bg-red-300 w-[300px] h-[50vh] relative flex justify-center items-center hover:scale-105">
-					<Image src="/images/panic-button.svg" alt="Botón de pánico" fill />
+				<div className="relative flex justify-center items-center hover:scale-105 transition-transform duration-300 ease-out cursor-pointer">
+					<Image src="/images/panic-button.svg" alt="Botón de pánico" width={250} height={250} />
 				</div>
-				{/* Patient Information */}
-				{emergenciesList.map((emergency) => (
-					<EmergencyCard key={emergency.emergencyId} userName={emergency.userName} userPhone={emergency.userPhone} emergencyId={emergency.emergencyId} />
-				))}
 			</div>
+
+			{/* Footer */}
+			<StrokeeLogo />
 		</main>
 	);
 }
