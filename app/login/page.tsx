1dc4e18a7b51100fecbe82a62c3b795e8908105f
Jose Gabriel Jiménez <josegabojimenez@gmail.com>
2025-02-25 19:11:56 -0500

    feat: create axios instance with interceptor to connect to backend, connect patient emergency contacts list to the backend

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -23,7 +23,7 @@ export default function Login() {
 			{isLoading ? (
 				<div className="w-6 h-6 border-2 border-customRed border-t-transparent rounded-full animate-spin" />
 			) : (
-				<LoginForm placeholder="Correo" />
+				<LoginForm placeholder="Correo " />
 			)}
 		</main>
 	);
