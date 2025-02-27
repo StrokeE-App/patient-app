f7786afb40db12a39c3ad39695fb55264b7f3158
Jose Gabriel Jiménez <74217610+josegabjimenez@users.noreply.github.com>
2025-02-26 20:33:05 -0500

    feat: "add emergency contact" integration to backend (#7)
    * feat: create axios instance with interceptor to connect to backend, connect patient emergency contacts list to the backend
    
    * feat: connect add emergency contact to the backend

--- app/emergency-contacts/add/page.tsx
+++ app/emergency-contacts/add/page.tsx
@@ -1,47 +1,78 @@
 'use client';
-
-import Button from '@/components/Button';
-import Input from '@/components/Input';
-import {EmergencyContact} from '@/types';
-import {ArrowBigLeft} from 'lucide-react';
+import {useState, useMemo} from 'react';
 import Link from 'next/link';
 import {useRouter} from 'next/navigation';
-import {useEffect, useState, useMemo} from 'react';
+
+// Components
+import toast from 'react-hot-toast';
+import Button from '@/components/Button';
+import Input from '@/components/Input';
+import {ArrowBigLeft} from 'lucide-react';
+
+// Context
+import {useAuth} from '@/context/AuthContext';
+
+// API
+import apiClient from '@/api/api';
 
 export default function AddEmergencyContactPage() {
 	const router = useRouter();
-	const [name, setName] = useState('');
-	const [phone, setPhone] = useState('');
+	const [firstName, setFirstName] = useState('');
+	const [lastName, setLastName] = useState('');
+	const [phoneNumber, setPhoneNumber] = useState('');
 	const [relationship, setRelationship] = useState('');
 	const [email, setEmail] = useState('');
-	const [contacts, setContacts] = useState<EmergencyContact[]>([]);
 
-	// Load contacts from localStorage
-	useEffect(() => {
-		const contacts = localStorage.getItem('emergencyContacts');
-		if (contacts) {
-			setContacts(JSON.parse(contacts));
-		}
-	}, []);
+	const {user} = useAuth();
 
 	// Disable button if any of the fields are empty
 	const isDisabled = useMemo(() => {
-		return !(name && phone && relationship && email);
-	}, [name, phone, relationship, email]);
+		return !(firstName && phoneNumber && relationship && email);
+	}, [firstName, phoneNumber, relationship, email]);
 
-	// Add new contact to localStorage and redirect to emergency contacts page
-	const handleAddContact = () => {
-		if (name && phone && relationship && email) {
-			const newContact = {
-				id: contacts.length + 1,
-				name,
-				phone,
-				relationship,
-				email,
-			};
-			setContacts([...contacts, newContact]);
-			localStorage.setItem('emergencyContacts', JSON.stringify([...contacts, newContact]));
-			router.push('/emergency-contacts');
+	// // Add new contact to localStorage and redirect to emergency contacts page
+	// const handleAddContact = () => {
+	// 	if (firstName && lastName && phoneNumber && relationship && email) {
+	// 		const newContact = {
+	// 			id: contacts.length + 1,
+	// 			firstName,
+	// 			phoneNumber,
+	// 			relationship,
+	// 			email,
+	// 		};
+	// 		setContacts([...contacts, newContact]);
+	// 		localStorage.setItem('emergencyContacts', JSON.stringify([...contacts, newContact]));
+	// 		router.push('/emergency-contacts');
+	// 	}
+	// };
+
+	// Add new contact to the database and redirect to emergency contacts page
+	const handleAddContact = async () => {
+		if (firstName && lastName && phoneNumber && relationship && email && user) {
+			// Validate phone number format (10 digits)
+			if (!/^\d{10}$/.test(phoneNumber)) {
+				toast.error('Número de teléfono inválido.');
+				return;
+			}
+
+			const loadingToast = toast.loading('Agregando Contacto de Emergencia...');
+			try {
+				await apiClient.post('/patient/emergency-contacts/add', {
+					patientId: user.uid,
+					contact: {
+						firstName,
+						lastName,
+						phoneNumber,
+						email,
+						relationship,
+					},
+				});
+				toast.success('Contacto de Emergencia agregado.', {id: loadingToast});
+				router.push('/emergency-contacts');
+			} catch (error) {
+				toast.error('Error al agregar el Contacto de Emergencia.', {id: loadingToast});
+				console.error(error);
+			}
 		}
 	};
 	return (
@@ -54,9 +85,10 @@ export default function AddEmergencyContactPage() {
 			<div className="flex flex-col items-center justify-center">
 				<h1 className="text-4xl font-bold text-center">Nuevo Contacto</h1>
 				<div className="flex flex-col items-center gap-4 mt-8 w-[90vw] max-w-[40rem]">
-					<Input type="text" placeholder="Nombre" value={name} onChange={(e) => setName(e.target.value)} required />
+					<Input type="text" placeholder="Nombre" value={firstName} onChange={(e) => setFirstName(e.target.value)} required />
+					<Input type="text" placeholder="Apellido" value={lastName} onChange={(e) => setLastName(e.target.value)} required />
 					<Input type="text" placeholder="Parentesco" value={relationship} onChange={(e) => setRelationship(e.target.value)} required />
-					<Input type="tel" placeholder="Teléfono" value={phone} onChange={(e) => setPhone(e.target.value)} required />
+					<Input type="tel" placeholder="Teléfono" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} required />
 					<Input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
 					<Button className="rounded-lg bg-customRed !text-customWhite" disabled={isDisabled} onClick={handleAddContact}>
 						Agregar Contacto
